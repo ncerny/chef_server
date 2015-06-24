2a3ad7f99391bbce5a073986f1ff8172c70c4135
mauricio silva <mauicio.silva@gmail.com>
2015-06-24 11:43:06 -0700

    adding topology as attribute to default provider

--- providers/default.rb
+++ providers/default.rb
@@ -1,6 +1,7 @@
 action :create do
   machine new_resource.name do
     recipe 'chef_server::_chef_server_core'
+    attributes  'chef_server' => { 'topology' => new_resource.topology } 
     machine_options new_resource.machine_options
   end
 end
