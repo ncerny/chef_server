6a62e310d05297b21ed405a13a874a0751b220ac
andy-dufour <adufour@chef.io>
2015-06-24 10:22:05 -0700

    Explicitly setting vm box

--- berks-cookbooks/chef_server/recipes/_chef_server_core.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: chef_server
-# Recipe:: _chef_server_core
-#
-# Copyright (c) 2015 The Authors, All Rights Reserved.
-
-chef_server_ingredient 'chef-server-core' do
-  reconfigure true
-end
