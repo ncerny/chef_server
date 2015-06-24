3235175159c4086b4c5526ddc585279046270f0b
Nathan Cerny <ncerny@chef.io>
2015-06-24 09:03:06 -0700

    Pass machine_options to underlying machine resource.

--- resources/default.rb
+++ resources/default.rb
@@ -4,3 +4,4 @@ default_action :create
 attribute :name,      kind_of: String, name_attribute: true
 attribute :roles,     kind_of: [Array, String], default: 'backend'
 attribute :addons,    kind_of: [Array, String], default: ''
+attribute :machine_options, kind_of: Object
